223268b2f14f1680394e87460ec46c2b833a32d8
iansteyn <iansteyn03@gmail.com>
2024-03-06 11:19:21 -0800

    Update conftest.py with flaskr tutorial code
    The flask docs tutorial on testing has code that is more compatible with our existing project code. Updated accordingly

--- Code/tests/conftest.py
+++ Code/tests/conftest.py
@@ -4,21 +4,49 @@
 Configuration file for unit testing our flask app.
 Contains fixtures that all tests will need.
 '''
+'''
+Starter Code sources:
+- [Flask docs tutorial: Test Coverage](https://flask.palletsprojects.com/en/3.0.x/tutorial/tests/) (newer commits)
+- [Getting Started With Testing in Flask](https://www.youtube.com/watch?v=RLKW7ZMJOf4) (older commits)
+'''
 # -------------------------------------------------
+import os
+import tempfile
+
 import pytest
-from weatherApp import create_app, db
+from weatherApp import create_app
+from weatherApp.db import get_db, init_db
+
+with open(os.path.join(os.path.dirname(__file__), 'data.sql'), 'rb') as f:
+    _data_sql = f.read().decode('utf8')
 
 @pytest.fixture()
 def app():
-    app = create_app("sqlite://")
+    # open a temporary file for test data
+    db_fd, db_path = tempfile.mkstemp()
+
+    # Create an app instance in test mode
+    app = create_app({
+        'TESTING': True,
+        'DATABASE': db_path
+    })
 
     with app.app_context():
-        db.init_db()
+        init_db()
+        get_db().executescript(_data_sql)
 
     yield app 
-    # everything after this would be teardown
+
+    # teardown
+    os.close(db_fd)
+    os.unlink(db_path)
 
 @pytest.fixture()
 def client(app):
     # allows us to simulate requests to the app
-    app.test_client()
+    return app.test_client()
+
+@pytest.fixture()
+def runner(app):
+    # creates a runner that can call the Click commands registered with the application
+    return app.test_cli_runner()
