9989cf731b3eb66a046a0f9af929c8f3edcc0098
iansteyn <iansteyn03@gmail.com>
2024-03-06 10:36:54 -0800

    create test_weatherApp.py

---
+++ Code/tests/test_weatherApp.py
@@ -0,0 +1,8 @@
+# ------------------------------------------------
+# test_weatherApp.py
+'''
+I created this file as part of a flask testing tutorial.
+In the future we will create several test files.
+'''
+# ------------------------------------------------
+
