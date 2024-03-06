722d8ff70799773364e23d5c7776d03e3b000614
iansteyn <iansteyn03@gmail.com>
2024-03-06 10:58:05 -0800

    add method: test_home()

--- Code/tests/test_weatherApp.py
+++ Code/tests/test_weatherApp.py
@@ -4,5 +4,14 @@
 I created this file as part of a flask testing tutorial.
 In the future we will create several test files.
 '''
+'''
+Starter Code sources:
+- [Getting Started With Testing in Flask](https://www.youtube.com/watch?v=RLKW7ZMJOf4)
+'''
 # ------------------------------------------------
 
+def test_home(client):
+    response = client.get("/")
+
+    # check that the page has something we know is going to be there
+    assert b"<title>Home</title>" in response.data
