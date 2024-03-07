54ec03a7644d4f66237701e0ce8b5946ebf18c88
iansteyn <iansteyn03@gmail.com>
2024-03-06 21:48:23 -0800

    bugfix in test_db.py
    Change 'flaskr' to 'weatherApp' in test_init_db_command

--- Code/tests/test_db.py
+++ Code/tests/test_db.py
@@ -39,7 +39,7 @@ def test_init_db_command(runner, monkeypatch):
     def fake_init_db():
         Recorder.called = True
 
-    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    monkeypatch.setattr('weatherApp.db.init_db', fake_init_db)
     result = runner.invoke(args=['init-db'])
     assert 'Initialized' in result.output
     assert Recorder.called
