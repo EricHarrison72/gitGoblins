74a461c8ed2681afdd029e850c61fabd0b3f2827
iansteyn <iansteyn03@gmail.com>
2024-03-13 16:06:20 -0700

    Split expected_dict into three fixtures
    For some reason the integration testing was messing with the fixture build, so I'm simplifying it while sacrificing self-documenting code

--- Code/tests/test_queries.py
+++ Code/tests/test_queries.py
@@ -7,11 +7,16 @@ Contains unit tests for queries.py
 import pytest
 from weatherApp.queries import get_weather_data
 
+'''
+Note: I had to split these dicts into 3 separate fixtures because the old fixture
+built incorrectly on GitHub Actions, causing integration tests to fail
+'''
+
+# Expected dict for: params passed, data exists
 @pytest.fixture()
-def expected_dict():
-    my_dict = {
-        # data directly from the csv
-        'params passed + data exists': {
+def expected_dict_1():
+    # data straight from csv
+    return {
             'city_name': 'Albury', 
             'date': '2008-12-01', 
             'temp_high': 22.9, 
@@ -20,9 +25,13 @@ def expected_dict():
             'raining': 'No', 
             'wind_speed': 44, 
             'wind_dir': 'W'
-        },
+        }
+
+# Expected dict for: params passed, no data exists
+@pytest.fixture()
+def expected_dict_2():
         # data for Albury starts in 2008, so there should be no data for 2007
-        'params passed + no data exists': {
+        return {
             'city_name': 'NO DATA for Albury on this date', 
             'date': '2007-12-01', 
             'temp_high': 0, 
@@ -31,8 +40,12 @@ def expected_dict():
             'raining': '?', 
             'wind_speed': 0, 
             'wind_dir': '?'
-        },
-        'none params passed': {
+        }
+
+# Expected dict for: 'None' params passed
+@pytest.fixture()
+def expected_dict_3():
+        return {
             'city_name': 'NO DATA', 
             'date': 'No Date', 
             'temp_high': 0, 
@@ -42,13 +55,15 @@ def expected_dict():
             'wind_speed': 0, 
             'wind_dir': '?'
         }
-    }
 
-    return my_dict
+def test_get_weather_data(app, expected_dict_1, expected_dict_2, expected_dict_3):
 
-def test_get_weather_data(app, expected_dict):
-    
     with app.app_context():
-        assert expected_dict['params passed + data exists'] == get_weather_data('Albury', '2008-12-01')
-        assert expected_dict['params passed + no data exists'] == get_weather_data('Albury', '2007-12-01')
-        assert expected_dict['none params passed'] == get_weather_data(None, None)
+        # params passed, data exists
+        assert expected_dict_1 == get_weather_data('Albury', '2008-12-01')
+
+        # params passed, no data exists
+        assert expected_dict_2 == get_weather_data('Albury', '2007-12-01')
+
+        #'None' params passed
+        assert expected_dict_3 == get_weather_data(None, None)
