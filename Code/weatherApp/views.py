151394ff378ad07f08e35ce33498a3b7324044f9
Eric Harrison <hericeh72@gmail.com>
2024-03-15 14:05:00 -0700

    Added function to determine which weather icon to use as a marker

--- Code/weatherApp/views.py
+++ Code/weatherApp/views.py
@@ -11,7 +11,7 @@ Start Code sources:
 - [Flask docs tutorial - Application Setup](https://flask.palletsprojects.com/en/3.0.x/tutorial/factory/)
 '''
 # ------------------------------------------------------
-from flask import render_template, Blueprint, request
+from flask import render_template, Blueprint, request, jsonify
 from . import db
 from .auth import login_required
 
@@ -65,3 +65,25 @@ def weather_summary():
 #@login_required
 def map():
     return render_template("map.html.jinja")
+
+#This page is used only to determine which weather icon to use on the map as a marker
+@views_bp.route('/api/weather_icon')
+def get_weather_icon():
+    city_name = request.args.get('cityName')
+    date = request.args.get('date')
+    
+    # Use the database connection to query weather data
+    db = db.get_db()
+    weather_data = db.execute('''
+        SELECT * FROM WeatherInstance
+        JOIN City ON WeatherInstance.cityId = City.cityId
+        WHERE City.cityName = ? AND WeatherInstance.date = ?
+    ''', (city_name, date)).fetchone()
+    
+    # Assuming `determine_icon_based_on_weather` is a function that takes weather data and returns an icon name
+    if weather_data:
+        icon_name = determine_icon_based_on_weather(weather_data)
+    else:
+        icon_name = "default_icon"  # Fallback icon if no weather data found
+    
+    return jsonify({'icon': icon_name})
