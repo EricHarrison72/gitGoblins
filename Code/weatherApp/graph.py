917e56fe09f7662f80f4e6e2b208ac045bddbc79
iansteyn <iansteyn03@gmail.com>
2024-03-16 13:19:09 -0700

    Create graph.py

---
+++ Code/weatherApp/graph.py
@@ -0,0 +1,10 @@
+# ------------------------------------------
+# graph.py
+'''
+Code for the graphs, using plotly.py
+'''
+'''
+Resources used:
+- [BugBytes, "Django & Plotly" video](https://www.youtube.com/watch?v=TcnWEQMT3_A)
+'''
+# ------------------------------------------
