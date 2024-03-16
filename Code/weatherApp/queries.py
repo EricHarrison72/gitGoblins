3c403240402c1ccbe599fe9c709dd74c20fe6269
iansteyn <iansteyn03@gmail.com>
2024-03-16 14:41:31 -0700

    Add get_temp_in_range() to queries.py
    SQL query used by graphs.py

--- Code/weatherApp/queries.py
+++ Code/weatherApp/queries.py
@@ -58,3 +58,20 @@ def get_weather_data (city_name, date):
         }
 
     return weather_data_dict
+
+# 
+def get_temp_in_range(city_name, start_date, end_date):
+
+    datb = db.get_db()
+
+    #SQL query to get data for specific city
+    #weather_data is an sqlite3 Row object
+    temp_in_range = datb.execute('''
+        SELECT City.cityName AS city_name, WeatherInstance.date, WeatherInstance.tempMax AS temp_high, 
+               WeatherInstance.tempMin AS temp_low
+        FROM WeatherInstance
+        JOIN City ON WeatherInstance.cityId = City.cityId
+        WHERE City.cityName = ? AND date BETWEEN ? AND ?
+    ''', (city_name,start_date, end_date)).fetchall()
+
+    return temp_in_range
