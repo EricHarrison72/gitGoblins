13640bd64b1b1e7b7709a71eb664bc45960b4d4b
iansteyn <iansteyn03@gmail.com>
2024-03-20 16:00:39 -0700

    Move core feature templates to new folder
    Map, weather summary, location select, and graphs moved to a folder called 'features' in templates, for file organization purposes

--- Code/weatherApp/views.py
+++ Code/weatherApp/views.py
@@ -37,23 +37,24 @@ def weather_summary():
 
     weather_dict = queries.get_weather_data(city_name, date)
     
-    return render_template("weather_summary.html.jinja", weather_dict=weather_dict)
+    return render_template("features/weather_summary.html.jinja", weather_dict=weather_dict)
 
 @views_bp.route('/map')
 #@login_required
 def map():
-    return render_template("map.html.jinja")
+    return render_template("features/map.html.jinja")
 
 @views_bp.route('/graph')
 def graph():
     figure_html = graphs.get_temp_figure_html()
-    return render_template("graph.html.jinja", figure_html = figure_html)
+    return render_template("features/graph_past.html.jinja", figure_html = figure_html)
 
 @views_bp.route('/location_select')
 #@login_required
 def location_select():
-    return render_template("location_select.html.jinja")
+    return render_template("features/location_select.html.jinja")
 
+# -----------------
 #This page is used only to determine which weather icon to use on the map as a marker
 @views_bp.route('/api/weather_icon')
 def get_weather_icon():
