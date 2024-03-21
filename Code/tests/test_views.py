74558431aeed82730459248fcfc324291a4c09a4
Eric Harrison <hericeh72@gmail.com>
2024-03-21 15:09:43 -0700

    Added login to test to improve coverage

--- Code/tests/test_views.py
+++ Code/tests/test_views.py
@@ -59,16 +59,30 @@ def test_weather_summary_not_authenticated(client, app):
 # Test if the map page renders successfully
 def test_map(client, app):
     with client:
+        # Register a new user
+        client.post('/auth/register', data={'email': 'test@gmail.com', 'password': 'a'})
+        # Attempt to log in with the registered user
+        response_login = client.post('/auth/login', data={'email': 'test@gmail.com', 'password': 'a'})
+        # Check if login was successful
+        assert response_login.status_code == 302  # Check if login was successful
+        
         # Access the map page
         response = client.get('/map')
-        assert response.status_code == 302
+        assert response.status_code == 200
         
 # Test if the location_select page renders successfully
 def test_location_select(client, app):
     with client:
+        # Register a new user
+        client.post('/auth/register', data={'email': 'test@gmail.com', 'password': 'a'})
+        # Attempt to log in with the registered user
+        response_login = client.post('/auth/login', data={'email': 'test@gmail.com', 'password': 'a'})
+        # Check if login was successful
+        assert response_login.status_code == 302  # Check if login was successful
+        
         # Access the location_select page
         response = client.get('/location_select')
-        assert response.status_code == 302
+        assert response.status_code == 200
         
 # Test if the weather_icon API returns a valid response
 def test_get_weather_icon(client, app):
