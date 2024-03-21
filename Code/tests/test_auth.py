66ac6bf08dd6a127b39bdc773bedc9cdaf6c7cef
Eric Harrison <hericeh72@gmail.com>
2024-03-21 16:07:32 -0700

    Added tests for all register and login errors to improve coverage on edge cases

--- Code/tests/test_auth.py
+++ Code/tests/test_auth.py
@@ -10,20 +10,40 @@ Start code sources:
 # --------------------------------------------------
 
 import bcrypt
-import pytest
-from flask import g, session
-from weatherApp import create_app
-from weatherApp.db import get_db, init_db
+from flask import session
+from weatherApp.db import get_db
 
 
 #The register view should render successfully on GET. On POST with valid form data, 
 # it should redirect to the login URL and the user’s data should be in the database.
 def test_register(client, app):
     assert client.get('/auth/register').status_code == 200
+    # Test registering user
     response = client.post(
         '/auth/register', data={'email': 'test@gmail.com', 'password': 'a'}
     )
+    # Check if it redirects to login page
     assert response.headers["Location"] == "/auth/login"
+    
+    response = client.post(
+        '/auth/register', data={'email': 'test@gmail.com', 'password': 'a'}
+    )
+    # Check if it redirects to login page
+    assert b'User with email test@gmail.com is already registered.' in response.data
+
+    # Test register with no email
+    response_no_email = client.post(
+        '/auth/register', data={'email': '', 'password': 'a'}
+    )
+    # Check for correct error message
+    assert b'Email is required.' in response_no_email.data
+
+    # Test register with no password
+    response_no_password = client.post(
+        '/auth/register', data={'email': 'test@gmail.com', 'password': ''}
+    )
+    # Check for correct error message
+    assert b'Password is required.' in response_no_password.data
 
     with app.app_context():
         assert get_db().execute(
@@ -49,6 +69,20 @@ def test_login(client, app):
     )
     # Check if it redirects to the home page
     assert response.headers["Location"] == "/"
+    
+    # Test login with no email
+    response_no_email = client.post(
+        '/auth/login', data={'email': '', 'password': 'a'}
+    )
+    # Check for correct error message
+    assert b'Incorrect email.' in response_no_email.data
+    
+    # Test login with incorrect password
+    response_no_password = client.post(
+        '/auth/login', data={'email': 'test_user@gmail.com', 'password': 'wrongpassword'}
+    )
+    # Check for correct error message
+    assert b'Incorrect password.' in response_no_password.data
 
     # Check if user ID 100 is stored in session
     with client:
