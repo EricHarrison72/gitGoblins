8338ac7c02c39de49ce425bbc5da965870fdacc6
iansteyn <iansteyn03@gmail.com>
2024-03-23 10:57:31 -0700

    Create testing files: submit.test.js, test_jinja_macros

---
+++ Code/tests/submit.test.js
@@ -0,0 +1,9 @@
+// submit.test.js
+// unit tests for submit.js
+
+//TODO: create mocks
+
+//TODO: 
+//test getDateFromSelection
+//test getCityFromSelection
+//test reloadPageWithArgs
---
+++ Code/tests/test_jinja_macros.py
@@ -0,0 +1,21 @@
+# ---------------------------------------------------
+# test_jinja_macros.py
+'''
+Contains unit tests for all jinja macros, which are
+kind of like functions and warrant testing.
+
+Currently all Jinja macros are in 'helpers/selectors.html.jinja'.
+'''
+# --------------------------------------------------
+
+# TODO fixtures?
+
+# TODO tests
+def test_select_date():
+    pass
+
+def test_select_single_location():
+    pass
+
+def test_select_multiple_locations():
+    pass
