5e9ef9ac4b4d32f109fe47f072d25926283cf6c8
iansteyn <iansteyn03@gmail.com>
2024-03-27 20:06:26 -0700

    Rework expected_dict fixture to remove unecessary comments
    This is for an old test, but based on feedback given by Rohit last week

--- Code/tests/test_queries.py
+++ Code/tests/test_queries.py
@@ -7,68 +7,58 @@ Contains unit tests for queries.py
 import pytest
 from weatherApp.queries import (
     get_weather_data,
-    get_temp_in_range,
+    #get_temp_in_range,
     add_space
 )
 
 # --------
 # FIXTURES
-'''
-Note: I had to split these dicts into 3 separate fixtures because the old fixture
-built incorrectly on GitHub Actions, causing integration tests to fail
-'''
 
-# Expected dict for: params passed, data exists
 @pytest.fixture()
-def expected_dict_1():
-    # data that I know is in test DB
+def expected_weather_dict():
     return {
-            'city_name': 'Springfield', 
-            'date': '2023-01-01', 
-            'temp_high': 10.0, 
-            'temp_low': -5.0, 
-            'rainfall': 0.0, 
-            'raining': 'No', 
-            'wind_speed': 30, 
+         # (data that I know is in test DB)
+        'params passed, data exists': {
+            'city_name': 'Springfield',
+            'date': '2023-01-01',
+            'temp_high': 10.0,
+            'temp_low': -5.0,
+            'rainfall': 0.0,
+            'raining': 'No',
+            'wind_speed': 30,
             'wind_dir': 'N',
             'cloud': 3
-        }
-
-# Expected dict for: params passed, no data exists
-@pytest.fixture()
-def expected_dict_2():
-        # There is no data for Albury in the test DB
-        return {
-            'city_name': 'NO DATA for Albury on this date', 
-            'date': '2007-12-01', 
-            'temp_high': 0, 
-            'temp_low': 0, 
-            'rainfall': 0.0, 
-            'raining': '?', 
-            'wind_speed': 0, 
+        },
+         # (There is no data for Albury in the test DB)
+        'params passed, no data exists': {
+             'city_name': 'NO DATA for Albury on this date',
+            'date': '2007-12-01',
+            'temp_high': 0,
+            'temp_low': 0,
+            'rainfall': 0.0,
+            'raining': '?',
+            'wind_speed': 0,
+            'wind_dir': '?',
+            'cloud': 0
+        },
+        '`None` params passed': {
+            'city_name': 'NO DATA',
+            'date': 'No Date',
+            'temp_high': 0,
+            'temp_low': 0,
+            'rainfall': 0.0,
+            'raining': '?',
+            'wind_speed': 0,
             'wind_dir': '?',
             'cloud': 0
         }
+    }
 
-# Expected dict for: 'None' params passed
-@pytest.fixture()
-def expected_dict_3():
-        return {
-            'city_name': 'NO DATA', 
-            'date': 'No Date', 
-            'temp_high': 0, 
-            'temp_low': 0, 
-            'rainfall': 0.0, 
-            'raining': '?', 
-            'wind_speed': 0, 
-            'wind_dir': '?',
-            'cloud': 0
-        }
 
 # Note
 '''
 Since sqlite queries return Sqlite3.Row objects that are hard to mock,
-it make the most sence to make the expected tables dictionaries and
+it makes the most sense to make the expected tables dictionaries and
 to then iterate over each item in both the expected dictionary and real
 row, testing equality for each pair of items instead of for the whole object.
 '''
@@ -83,30 +73,30 @@ def expected_temp_table():
 # --------
 # TESTS
 
-def test_get_weather_data(app, expected_dict_1, expected_dict_2, expected_dict_3):
+def test_get_weather_data(app, expected_weather_dict):
 
     with app.app_context():
-        # params passed, data exists
-        assert expected_dict_1 == get_weather_data('Springfield', '2023-01-01')
+        assert (expected_weather_dict['params passed, data exists']
+                == get_weather_data('Springfield', '2023-01-01'))
 
-        # params passed, no data exists
-        assert expected_dict_2 == get_weather_data('Albury', '2007-12-01')
+        assert (expected_weather_dict['params passed, no data exists']
+                == get_weather_data('Albury', '2007-12-01'))
 
-        #'None' params passed
-        assert expected_dict_3 == get_weather_data(None, None)
+        assert (expected_weather_dict['`None` params passed']
+                == get_weather_data(None, None))
 
 def test_add_space():
       assert add_space('AliceSprings') == 'Alice Springs'
       assert add_space('NewYorkCity') == 'New York City'
 
-def test_get_temp_in_range(app, expected_temp_table):
+# def test_get_temp_in_range(app, expected_temp_table):
     
-    with app.app_context():
-        real_temp_table = get_temp_in_range('Springfield', '2023-01-01', '2023-01-03')
+#     with app.app_context():
+#         real_temp_table = get_temp_in_range('Springfield', '2023-01-01', '2023-01-03')
 
-    for i in range(len(expected_temp_table)):
-        for key in expected_temp_table[i].keys():
-            assert expected_temp_table[i][key] == real_temp_table[i][key]
+#     for i in range(len(expected_temp_table)):
+#         for key in expected_temp_table[i].keys():
+#             assert expected_temp_table[i][key] == real_temp_table[i][key]
 
 # --------
 
