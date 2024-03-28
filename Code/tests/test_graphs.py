4f872d616b143250f4f769952c65db9b57bcbb05
iansteyn <iansteyn03@gmail.com>
2024-03-27 22:39:35 -0700

    Create file test_graphs.py

---
+++ Code/tests/test_graphs.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------
+# test_graphs.py
+'''
+Unit tests for the classes and methods in graphs.py
+Methodology:
+- Create instances of each class, test only that instance variables are what you expect it to be
+- see what coverage is after that and then move forward
+
+- also test get_fig
+'''
+# -------------------------------------------------
