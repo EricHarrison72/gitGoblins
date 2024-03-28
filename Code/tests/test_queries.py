7994beb9e2588e66dfbed72cda949c73cf0c08cc
iansteyn <iansteyn03@gmail.com>
2024-03-27 21:45:12 -0700

    Add valid_info fixture and test for get_data_in_range
    Currently only tests for valid queries as invalid queries are not responded to yet

--- Code/tests/test_queries.py
+++ Code/tests/test_queries.py
@@ -11,10 +11,25 @@ from weatherApp.queries import (
     _generate_column_script,
     add_space
 )
+# ==================================
+# SMALLER TESTS
+# -------------
+def test_add_space():
+      assert add_space('AliceSprings') == 'Alice Springs'
+      assert add_space('NewYorkCity') == 'New York City'
 
-# --------
-# FIXTURES
+def test_generate_column_script(app):
+    expected_column_script_A = 'Column1,Column2,Column3'
+    assert (_generate_column_script(['Column1', 'Column2', 'Column3'])
+            == expected_column_script_A)
+    
+    expected_column_script_B = 'Column1,Column4'
+    assert (_generate_column_script(['Column1', 'Column4'])
+            == expected_column_script_B)
 
+# =======================================
+# get_weather_data FIXTURES
+# -------------------------
 @pytest.fixture()
 def expected_weather_dict():
     return {
@@ -55,25 +70,8 @@ def expected_weather_dict():
         }
     }
 
-
-# Note
-'''
-Since sqlite queries return Sqlite3.Row objects that are hard to mock,
-it makes the most sense to make the expected tables dictionaries and
-to then iterate over each item in both the expected dictionary and real
-row, testing equality for each pair of items instead of for the whole object.
-'''
-@pytest.fixture()
-def expected_temp_table():
-     return [
-          {'date':'2023-01-01', 'temp_low': -5.0, 'temp_high' : 10.0},
-          {'date':'2023-01-02', 'temp_low': -3.0, 'temp_high' : 20.0},
-          {'date':'2023-01-03', 'temp_low': -2.0, 'temp_high' : 30.0}
-     ]
-
-# --------
-# TESTS
-
+# get_weather_data TESTS
+# ----------------------
 def test_get_weather_data(app, expected_weather_dict):
 
     with app.app_context():
@@ -86,10 +84,7 @@ def test_get_weather_data(app, expected_weather_dict):
         assert (expected_weather_dict['`None` params passed']
                 == get_weather_data(None, None))
 
-def test_add_space():
-      assert add_space('AliceSprings') == 'Alice Springs'
-      assert add_space('NewYorkCity') == 'New York City'
-
+# =======================================
 # TODO
 '''
 nontesting:
@@ -101,22 +96,78 @@ testing:
     - 2. without valid city and dates - check error response
 - for invalid column - check error response? maybe
 '''
-# def test_get_data_in_range(app, expected_temp_table):
-    
-#     with app.app_context():
-#         real_temp_table = get_temp_in_range('Springfield', '2023-01-01', '2023-01-03')
+# get_data_in_range FIXTURES
+# -------------------------
+class ValidInfo:
+    def __init__(
+        self,
+        city_and_dates,
+        multiple_cols,
+        single_col,
+        expected_mult_col_table,
+        expected_single_col_table
+    ):
+        self.city_and_dates = city_and_dates 
+        self.multiple_cols = multiple_cols
+        self.single_col = single_col
+        self.expected_mult_col_table = expected_mult_col_table
+        self.expected_single_col_table = expected_single_col_table
 
-#     for i in range(len(expected_temp_table)):
-#         for key in expected_temp_table[i].keys():
-#             assert expected_temp_table[i][key] == real_temp_table[i][key]
+@pytest.fixture()
+def valid_info():
+    multiple_cols = ['tempMin', 'tempMax']
+    single_col = ['rainfall']
 
-def test_generate_column_script(app):
-    expected_column_script_A = 'Column1,Column2,Column3'
-    assert (_generate_column_script(['Column1', 'Column2', 'Column3'])
-            == expected_column_script_A)
-    
-    expected_column_script_B = 'Column1,Column4'
-    assert (_generate_column_script(['Column1', 'Column4'])
-            == expected_column_script_B)
-# --------
+    city_and_dates = {
+        'city_name': 'Springfield',
+        'start_date': '2023-01-01',
+        'end_date': '2023-01-03'
+    }
+    expected_mult_col_table = [
+        {'date':'2023-01-01', 'tempMin': -5.0, 'tempMax' : 10.0},
+        {'date':'2023-01-02', 'tempMin': -3.0, 'tempMax' : 'NA'},
+        {'date':'2023-01-03', 'tempMin': 'NA', 'tempMax' : 30.0}
+    ]
+    expected_single_col_table= [
+        {'date':'2023-01-01', 'rainfall': 0.0},
+        {'date':'2023-01-02', 'rainfall': 5.0},
+        {'date':'2023-01-03', 'rainfall': 'NA'}
+    ]
 
+    return ValidInfo(
+        city_and_dates,
+        multiple_cols,
+        single_col,
+        expected_mult_col_table,
+        expected_single_col_table
+    )
+
+# get_data_in_range TESTS
+#------------------------
+def test_get_data_in_range__valid_query(app, valid_info):
+
+    with app.app_context():
+        real_mult_col_table = get_data_in_range(valid_info.multiple_cols, valid_info.city_and_dates)
+        real_single_col_table = get_data_in_range(valid_info.single_col, valid_info.city_and_dates)
+
+    num_rows = len( valid_info.expected_mult_col_table)
+
+    for row in range(num_rows):
+        for key in valid_info.expected_mult_col_table [row].keys():
+            assert (
+                valid_info.expected_mult_col_table [row][key]
+                == real_mult_col_table [row][key]
+            )
+
+        for key in valid_info.expected_single_col_table [row].keys():
+            assert (
+                valid_info.expected_single_col_table [row][key]
+                == real_single_col_table [row][key]
+            )
+
+def test_get_data_in_range__invalid_query(app, expected_temp_table):
+    '''
+    Test not yet implemented because responding to invalid queries is not
+    yet part of get_data_in_range (this is a TDD reminder to do that tho).
+    '''
+    pass
