c81af442c1253752004550bdd3839a4139ed0a09
Chase Winslow <chasew13579@gmail.com>
2024-03-29 13:49:35 -0700

    All fields added

--- Code/weatherApp/admin.py
+++ Code/weatherApp/admin.py
@@ -10,22 +10,49 @@ def admin():
         datb = db.get_db()
         if request.method == 'POST':
             city_id = int(request.form['city_id'])
-            temp_high = float(request.form['temp_high'])
-            specified_date = request.form['date']  # Set date using specified format
-            print(city_id)
-            print(temp_high)
-            # Update high temperature using UPDATE statement
+            temp_min = float(request.form['tempMin'])
+            temp_max = float(request.form['tempMax'])
+            specified_date = request.form['date']  
+            sunshine_value = float(request.form['sunshine'])
+            rainfall_value = float(request.form['rainfall'])
+            evaporation_value = float(request.form['evaporation'])
+            cloud9am_value = int(request.form['cloud9am'])
+            cloud3pm_value = int(request.form['cloud3pm'])
+            pressure9am_value = float(request.form['pressure9am'])
+            pressure3pm_value = float(request.form['pressure3pm'])
+            humidity9am_value = int(request.form['humidity9am'])
+            humidity3pm_value = int(request.form['humidity3pm'])
+            windGustSpeed_value = int(request.form['windGustSpeed'])
+            windGustDir_value = request.form['windGustDir']
+            windSpeed9am_value = int(request.form['windSpeed9am'])
+            windSpeed3pm_value = int(request.form['windSpeed3pm'])
+            windDir9am_value = request.form['windDir9am']
+            windDir3pm_value = request.form['windDir3pm']
+            rainToday_value = bool(request.form['rainToday'])
+            rainTomorrow_value = bool(request.form['rainTomorrow'])
+
+            # Fetch cityName from cityId
+            cursor = datb.execute("SELECT cityName FROM City WHERE cityId = ?", (city_id,))
+            city_name = cursor.fetchone()[0]
+
+            # Update all columns in WeatherInstance table
             datb.execute('''
                 UPDATE WeatherInstance
-                SET tempMax = ?
+                SET tempMin = ?, tempMax = ?, sunshine = ?, rainfall = ?, evaporation = ?, cloud9am = ?, cloud3pm = ?,
+                    pressure9am = ?, pressure3pm = ?, humidity9am = ?, humidity3pm = ?, windGustSpeed = ?,
+                    windGustDir = ?, windSpeed9am = ?, windSpeed3pm = ?, windDir9am = ?, windDir3pm = ?,
+                    rainToday = ?, rainTomorrow = ?
                 WHERE cityId = ? AND date = ?
-            ''', (temp_high, city_id, specified_date))
+            ''', (temp_min, temp_max, sunshine_value, rainfall_value, evaporation_value, cloud9am_value, cloud3pm_value,
+                  pressure9am_value, pressure3pm_value, humidity9am_value, humidity3pm_value, windGustSpeed_value,
+                  windGustDir_value, windSpeed9am_value, windSpeed3pm_value, windDir9am_value, windDir3pm_value,
+                  rainToday_value, rainTomorrow_value, city_id, specified_date))
 
             # Commit the changes to the database
             datb.commit()
 
-            # Redirect to weather_summary route with cityId and date parameters
-            return redirect(url_for('views.weather_summary', city_id=city_id, date=specified_date))
+            # Redirect to weather_summary route with cityName and date parameters
+            return redirect(url_for('views.weather_summary', city_name=city_name, date=specified_date))
     except ValueError as ve:
         error_message = 'Invalid input value: {}'.format(str(ve))
         return jsonify({'error': error_message}), 400
