1f8a05faf8102f628fad7fdc0de9dc14487b9b19
Chase Winslow <chasew13579@gmail.com>
2024-03-29 13:53:46 -0700

    updates max and min and redirects to weather sum page on inputted location

--- Code/weatherApp/admin.py
+++ Code/weatherApp/admin.py
@@ -13,23 +13,7 @@ def admin():
             temp_min = float(request.form['tempMin'])
             temp_max = float(request.form['tempMax'])
             specified_date = request.form['date']  
-            sunshine_value = float(request.form['sunshine'])
-            rainfall_value = float(request.form['rainfall'])
-            evaporation_value = float(request.form['evaporation'])
-            cloud9am_value = int(request.form['cloud9am'])
-            cloud3pm_value = int(request.form['cloud3pm'])
-            pressure9am_value = float(request.form['pressure9am'])
-            pressure3pm_value = float(request.form['pressure3pm'])
-            humidity9am_value = int(request.form['humidity9am'])
-            humidity3pm_value = int(request.form['humidity3pm'])
-            windGustSpeed_value = int(request.form['windGustSpeed'])
-            windGustDir_value = request.form['windGustDir']
-            windSpeed9am_value = int(request.form['windSpeed9am'])
-            windSpeed3pm_value = int(request.form['windSpeed3pm'])
-            windDir9am_value = request.form['windDir9am']
-            windDir3pm_value = request.form['windDir3pm']
-            rainToday_value = bool(request.form['rainToday'])
-            rainTomorrow_value = bool(request.form['rainTomorrow'])
+            
 
             # Fetch cityName from cityId
             cursor = datb.execute("SELECT cityName FROM City WHERE cityId = ?", (city_id,))
@@ -38,15 +22,9 @@ def admin():
             # Update all columns in WeatherInstance table
             datb.execute('''
                 UPDATE WeatherInstance
-                SET tempMin = ?, tempMax = ?, sunshine = ?, rainfall = ?, evaporation = ?, cloud9am = ?, cloud3pm = ?,
-                    pressure9am = ?, pressure3pm = ?, humidity9am = ?, humidity3pm = ?, windGustSpeed = ?,
-                    windGustDir = ?, windSpeed9am = ?, windSpeed3pm = ?, windDir9am = ?, windDir3pm = ?,
-                    rainToday = ?, rainTomorrow = ?
+                SET tempMin = ?, tempMax = ?
                 WHERE cityId = ? AND date = ?
-            ''', (temp_min, temp_max, sunshine_value, rainfall_value, evaporation_value, cloud9am_value, cloud3pm_value,
-                  pressure9am_value, pressure3pm_value, humidity9am_value, humidity3pm_value, windGustSpeed_value,
-                  windGustDir_value, windSpeed9am_value, windSpeed3pm_value, windDir9am_value, windDir3pm_value,
-                  rainToday_value, rainTomorrow_value, city_id, specified_date))
+            ''', (temp_min, temp_max, city_id, specified_date))
 
             # Commit the changes to the database
             datb.commit()
