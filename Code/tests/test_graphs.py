c356913fe95fbe5bbd3d54beba65143eb95c1ac1
iansteyn <iansteyn03@gmail.com>
2024-03-30 11:36:20 -0700

    Class-structurify the graph tests EVEN more
    The structure is now complete, now I just need to write all the tests

--- Code/tests/test_graphs.py
+++ Code/tests/test_graphs.py
@@ -2,6 +2,9 @@
 # test_graphs.py
 '''
 Unit tests for the classes and methods in graphs.py
+Note that tests are separated out so that they show up
+as individual tests in terminal and testing tab.
+
 The Plan:
 - Create instances of each class, test only that instance variables are what you expect them to be
 - test for: range has no missing values
@@ -12,35 +15,17 @@ The Plan:
 - also test get_fig
 '''
 # --------------------------------------------------
-# ==================================================
-# ##################################################
-# &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-# \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-# /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\
-# __________________________________________________
-# ..................................................
-# **************************************************
-# mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# ++++++++++++++++++++++++++++++++++++++++++++++++++
-# ::::::::::::::::::::::::::::::::::::::::::::::::::
-# """"""""""""""""""""""""""""""""""""""""""""""""""
 import pytest
 from weatherApp.graphs import (
     get_graph_html,
+    WeatherGraph,
     TemperatureGraph,
     RainGraph,
     WindGraph
 )
 
-# class Fig:
-#     def __init__(self):
-#         #......
-
-#     def:
-
+# GENERAL FIXTURES & SUPPORT
+# --------------------------
 class ExpectedGraph:
     def __init__(
         self,
@@ -52,18 +37,53 @@ class ExpectedGraph:
         self.dataframe = dataframe
         self.fig = fig
 
+class GraphTest():
+    def __init__(self, expected: ExpectedGraph, real: WeatherGraph):
+        self.expected = expected
+        self.real = real
+
+    def test_dataframe(self):
+        '''
+        By checking dataframe attribute, we check the whole _initialize_dataframe sequence... 
+        - checks that data is correct (_fetch_and_convert_data)
+        - checks that columns are properly renamed (_rename_columns)
+        - checks that NA data is properly handled (_handle_missing_data))
+        '''
+        expected_df = self.expected.dataframe
+        real_df = self.real.dataframe
+
+        for col in expected_df.keys():
+            for row in range( len(expected_df[col])):
+                assert (expected_df[col][row] == real_df[col][row])
+                
+    def test_figure(self):
+        assert 1 == 2
+
+    def test_get_html(self):
+        pass
+
+    def test_get_city_name(self):
+        pass
+
 @pytest.fixture()
-def expected_graph():
-    city_and_dates = {
+def given_city_and_dates():
+    return {
         'city_name': 'Springfield',
         'start_date': '2023-01-01',
         'end_date': '2023-01-03'
     }
+# ======================================
+
+# TEMPERATURE GRAPH - FIXTURES & SUPPORT
+# --------------------------------------
+@pytest.fixture()
+def expected_temp_graph(given_city_and_dates):
+    city_and_dates = given_city_and_dates
 
     expected_dataframe = {
         'Date': ['2023-01-01', '2023-01-02', '2023-01-03'],
-        'Low':  [-5.0        , -3.0        , 0],
-        'High': [10.0        , 0           , 30.0]
+        'Low':  [-5.0        , -3.0        , 0           ],
+        'High': [10.0        , 0           , 30.0        ]
     }
     expected_fig = None
 
@@ -73,35 +93,31 @@ def expected_graph():
         expected_fig
     )
 
-# @pytest.fixture()
-# def real_temperature_graph():
-
-#     return TemperatureGraph(city_and_dates)
-
-def test_TemperatureGraph_vars(app, expected_graph):
+@pytest.fixture()
+def real_temp_graph(app, given_city_and_dates):
     with app.app_context():
-        real_temp_graph = TemperatureGraph(expected_graph.city_and_dates)
+        return TemperatureGraph(given_city_and_dates)
 
-    assert real_temp_graph.city_and_dates == expected_graph.city_and_dates # not really necessary but a good starting point for tests
-    
-    # Check dataframe (TODO: make this its own test)
-    '''
-    By checking dataframe attribute, we check the whole _initialize_dataframe sequence... 
-    - checks that data is correct (_fetch_and_convert_data)
-    - checks that columns are properly renamed (_rename_columns)
-    - checks that NA data is properly handled (_handle_missing_data))
-    '''
-    for col in expected_graph.dataframe.keys():
-        for row in range( len(expected_graph.dataframe[col])):
-            assert expected_graph.dataframe[col][row] == real_temp_graph.dataframe[col][row]
+@pytest.fixture()
+def temp_graph_tests(expected_temp_graph, real_temp_graph):
+    return GraphTest(expected_temp_graph, real_temp_graph)
 
-    # assert temp_fig.fig ...
+# TEMPERATURE GRAPH TESTS
+# -----------------------
+def test_TemperatureGraph_dataframe(temp_graph_tests):
+    temp_graph_tests.test_dataframe()
 
-# Should this be two different funcs?
-def test_TemperatureGraph_funcs(app):
-    # assert temp_fig.get_html()...
-    # assert temp_fig.get_city_name()...
-    pass
+def test_TemperatureGraph_figure(temp_graph_tests):
+    temp_graph_tests.test_figure()
+
+def test_TemperatureGraph_get_html(temp_graph_tests):
+    temp_graph_tests.test_get_html()
+
+def test_TemperatureGraph_get_city_name(temp_graph_tests):
+    temp_graph_tests.test_get_city_name()
+
+
+
 
 # TODO
 '''
