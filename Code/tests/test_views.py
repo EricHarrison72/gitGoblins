77dfb0584ccc84ffead4ac9b47ebdd777a3aeafe
iansteyn <iansteyn03@gmail.com>
2024-04-03 21:33:19 -0700

    re-wrote rest of view tests
    But I realized this branched off main before graph_update got merged? painful

--- Code/tests/test_views.py
+++ Code/tests/test_views.py
@@ -1,25 +1,16 @@
 # ------------------------------------------------
 # test_views.py
 '''
-This currently exists to test (mostly front end stuff) in views.py
-In the future we will create several test files. (see test_example)
+Contains unit tests for the views.py
 '''
 '''
 Starter Code sources:
-- [Getting Started With Testing in Flask](https://www.youtube.com/watch?v=RLKW7ZMJOf4)
-'''
-'''
-Refactoring notes:
-- we already have tests users in the test db... do we really need to register a new user for each test??
+- [Flask Docs - test client](https://flask.palletsprojects.com/en/3.0.x/testing/#sending-requests-with-the-test-client)
 '''
 # ------------------------------------------------
 import pytest
 from weatherApp import views
 from weatherApp.db import get_db
-
-# HTTP status codes
-PAGE_OK = 200
-PAGE_FOUND = 302
 
 # THE MAIN TEST LOGIC IS WRITTEN HERE
 class ViewTests:
@@ -29,17 +20,14 @@ class ViewTests:
             assert view_response.status == '302 FOUND'
             assert view_response.location.endswith('/auth/login')
 
-    def test_authenticated(app, client, url, expected_html_elem):
-        with app.app_context():
-            with client:
+    def test_authenticated(client, url, expected_html_elem):
+        with client:
+            register_test_user(client)
+            login_test_user(client)
 
-                register_test_user(client)
-                login_test_user(client)
-
-                view_response = client.get( url)
-
-                assert view_response.status == '200 OK'
-                assert expected_html_elem.encode("utf-8") in view_response.data 
+            view_response = client.get( url)
+            assert view_response.status == '200 OK'
+            assert expected_html_elem.encode("utf-8") in view_response.data 
 
 # Helpers for test_authenticated
 def register_test_user(client):
@@ -60,112 +48,86 @@ def login_test_user(client):
             'password': 'a'
         }
     )
-# ----------------------
 
 # THE TESTS (for reals)
 # -------------------
-'''
+'''NOTE 1
 The reason each test is seperated out like this (even though it
 seems like it violates DRY) is because the client fixture needs
 to get torn down and reset between tests.
 '''
+'''NOTE 2
+Note the format of expected_html_elem: 'Page Title\n -'.
+This element is expected in the page because that is consistently
+the format in the title blocks of the templates. The following 
+alternatives are less reliable:
+    - 'Page Title' ... this might show up on pages other than the one you're testing
+    - '<h1>Page Title</h1>' or other tags ... formatting within tags varies between templates
+'''
+# HOME
+def test_index__not_authenticated(client):
+    ViewTests.test_not_authenticated(client, '/')
+
+# TODO @Chase or @Eric why does this fail??
+def test_index__authenticated(client):
+    ViewTests.test_authenticated(client, '/', 'Home\n -')
+
+# FEATURES
 def test_weather_summary__not_authenticated(client):
     ViewTests.test_not_authenticated(client, '/weather_summary')
 
-def test_weather_summary__authenticated(app, client):
-    ViewTests.test_authenticated(app, client,'/weather_summary', '<h1>\n  Weather Summary')
+def test_weather_summary__authenticated(client):
+    ViewTests.test_authenticated(client,'/weather_summary', 'Weather Summary\n -')
 
-def test_map__not_authenticated(client, app):
+def test_map__not_authenticated(client):
     ViewTests.test_not_authenticated(client, '/map')
 
-############################################################
-# Test if the location_select page renders successfully
-def test_location_select(client, app):
-    with client:
-        # Register a new user with valid data including city_id
-        response = client.post('/auth/register', data={
-            'email': 'test@gmail.com',
-            'password': 'a',
-            'city_id': '1'  # Assuming city_id is provided in the form data
-        })
-        assert response.status_code == 302  # Check if registration is successful and redirects
+def test_map__authenticated(client):
+    ViewTests.test_authenticated(client, '/map', 'Map\n -') 
 
-        # Access the location_select page
-        response = client.get('/location_select')
-        assert response.status_code == 302
-        
-# Test if the weather_icon API returns a valid response
-def test_get_weather_icon(client, app):
+def test_graph__not_authenticated(client):
+    ViewTests.test_not_authenticated(client, '/graph')
+
+def test_graph__authenticated(client):
+    ViewTests.test_authenticated(client, '/graph', 'Graph\n -')
+
+# OTHER
+def test_settings__not_authenticated(client):
+    ViewTests.test_not_authenticated(client, '/settings')
+
+def test_settings__authenticated(client):
+    ViewTests.test_authenticated(client, '/settings', 'Settings\n -')
+
+def test_get_weather_icon(client):
     with client:
-        # Access the weather_icon API
         response = client.get('/api/weather_icon?cityName=Sydney&date=2024-03-20')
-        assert response.status_code == 200
-        
-# Test if the index page renders successfully
-def test_index(client, app):
-    with client:
-        # Register a new user with valid data including city_id
-        response = client.post('/auth/register', data={
-            'email': 'test@gmail.com',
-            'password': 'a',
-            'city_id': '1'  # Assuming city_id is provided in the form data
-        })
-        assert response.status_code == 302  # Check if registration is successful and redirects
+        assert response.status == '200 OK'
 
-        # Access the index page
-        response = client.get('/')
-        assert response.status_code == 302
-   #graph.py is currently being worked on so this test isn't complete     
-"""
-def test_graph(client, app):
-   with client:
-      response = client.get('/graph')
-      assert response.status_code == 302
-   """
-# Test if the settings page renders successfully
-def test_settings(client, app):
-    with client:
-        # Register a new user with valid data including city_id
-        response = client.post('/auth/register', data={
-            'email': 'test@gmail.com',
-            'password': 'a',
-            'city_id': '1'  # Assuming city_id is provided in the form data
-        })
-        assert response.status_code == 302  # Check if registration is successful and redirects
+# TODO THIS TEST SHOULDN'T BE HERE
+'''
+The whole organization of user settings needs to be addressed in another branch
+  -- the fix_view_tests branch
+'''
+# def test_update_user_settings(client, app):
+#     with client:
+#         # Register a new user with valid data including city_id
+#         response = client.post('/auth/register', data={
+#             'email': 'test@gmail.com',
+#             'password': 'a',
+#             'city_id': '1'  # Assuming city_id is provided in the form data
+#         })
+#         assert response.status_code == 302  # Check if registration is successful and redirects
 
-        # Log in the registered user
-        response_login = client.post('/auth/login', data={
-            'email': 'test@gmail.com',
-            'password': 'a'
-        })
-        assert response_login.status_code == 302  # Check if login is successful and redirects
+#         # Log in the registered user
+#         response_login = client.post('/auth/login', data={
+#             'email': 'test@gmail.com',
+#             'password': 'a'
+#         })
+#         assert response_login.status_code == 302  # Check if login is successful and redirects
 
-        # Access the settings page
-        response_settings = client.get('/settings')
-        assert response_settings.status_code == 200  # Check if the settings page is accessible
-
-   
-   # Test updating user settings
-def test_update_user_settings(client, app):
-    with client:
-        # Register a new user with valid data including city_id
-        response = client.post('/auth/register', data={
-            'email': 'test@gmail.com',
-            'password': 'a',
-            'city_id': '1'  # Assuming city_id is provided in the form data
-        })
-        assert response.status_code == 302  # Check if registration is successful and redirects
-
-        # Log in the registered user
-        response_login = client.post('/auth/login', data={
-            'email': 'test@gmail.com',
-            'password': 'a'
-        })
-        assert response_login.status_code == 302  # Check if login is successful and redirects
-
-        # Update user settings
-        response_update = client.post('/settings', data={
-            'emailList': 'on',
-            'cityId': '2'  # Assuming new city_id
-        })
-        assert response_update.status_code == 302  # Check if update is successful and redirects
+#         # Update user settings
+#         response_update = client.post('/settings', data={
+#             'emailList': 'on',
+#             'cityId': '2'  # Assuming new city_id
+#         })
+#         assert response_update.status_code == 302  # Check if update is successful and redirects
